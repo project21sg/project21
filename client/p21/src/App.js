1d8ce5b6c1734ad1c207abdbf73f7dbe04dda4ec
Ze Xuan <syltaris@gmail.com>
2018-10-20 16:39:24 +0800

    add blueprint.js and link up css files

--- client/p21/src/App.js
+++ client/p21/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import {
+    Button
+} from '@blueprintjs/core';
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-      
-        
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <Button icon="add"> LOL</Button>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/p21/src/index.js
+++ client/p21/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import '../node_modules/normalize.css/normalize.css';
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
