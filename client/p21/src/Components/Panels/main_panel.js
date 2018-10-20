9eebb5dc47d649b7e60fd68ed135f229d9d87176
Ze Xuan <syltaris@gmail.com>
2018-10-20 18:00:14 +0800

    added panels for patients view

---
+++ client/p21/src/Components/Panels/main_panel.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+    Button,
+    Tab,
+    Tabs,
+    Card
+} from '@blueprintjs/core';
+
+class MainPanel extends Component {
+    render() {
+        return(
+            <div>
+                now
+            </div>
+        );
+    }
+}
+
+export default MainPanel;
--- client/p21/src/Views/Patients/view_patients.js
+++ client/p21/src/Views/Patients/view_patients.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import {
-    Navbar, 
     Button,
-    Alignment
+    Tab,
+    Tabs,
+    Card
 } from '@blueprintjs/core';
 
+import MainPatientPanel from '../../Components/Panels/main_panel';
+
 class PatientsView extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            selectedTabId: "ng"
+        }
+
+        this.handleTabChange = this.handleTabChange.bind(this);
+    }
+
+    handleTabChange(e) {
+        this.setState((prevState) => {
+            return {
+                selectedTabId: e.selectedTabId
+            };
+        })
+    }
+
     render() {
+        var s = this.state;
         return(
             <div>
-                yeah
+                <Tabs vertical id="TabsExample" onChange={this.handleTabChange} selectedTabId={s.selectedTabId} style={{width: '10%'}}>
+                    <Tab id="ng" title="Angular" panel={<MainPatientPanel />}/>
+                    <Tab id="mb" title="Ember" />
+                    <Tab id="rx" title="React"  />
+                    <Tab id="bb" title="Backbone" />
+                    <Tabs.Expander />
+                    <input className="bp3-input" type="text" placeholder="Search..." />
+                </Tabs>
             </div>
         );
     }
