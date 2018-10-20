1c66a57f1b1d84f53d2078c65dcde16c651c29c3
Ze Xuan <syltaris@gmail.com>
2018-10-20 15:53:55 +0800

    ignore root misc folder/files

--- client/p21/src/App.js
+++ client/p21/src/App.js
@@ -1,37 +1,12 @@
 import React, { Component } from 'react';
-import { 
-  AppBar, 
-  Toolbar,
-  SvgIcon,
-  IconButton,
-  Typography 
-} from '@material-ui/core';
-import {
-  Menu
-} from '@material-ui/icons';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <AppBar 
-          position="static"
-          color="primary">
-          <Toolbar>
-            <IconButton 
-              color="inherit"
-              aria-label="Open drawer">
-              <SvgIcon>
-                <Menu />
-              </SvgIcon>
-            </IconButton>
-            <Typography 
-              color="inherit">
-              Project 21
-            </Typography>
-          </Toolbar>
-
-        </AppBar>
+      
+        
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
