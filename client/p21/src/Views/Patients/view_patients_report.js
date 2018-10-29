95ffef84f345dbee4f95b7d516369bf1e13088d1
Ze Xuan <syltaris@gmail.com>
2018-10-30 00:05:27 +0800

    make mock graph upload

--- client/p21/src/Views/Patients/view_patients_report.js
+++ client/p21/src/Views/Patients/view_patients_report.js
@@ -6,6 +6,8 @@ import {
     Card,
     Text
 } from '@blueprintjs/core';
+import {LineChart, Line, XAxis, YAxis, CartesianGrid} from 'recharts';
+import XLSX from 'xlsx';
 
 class PatientReportsView extends Component {
     constructor(props) {
@@ -18,7 +20,8 @@ class PatientReportsView extends Component {
 
     componentWillReceiveProps(props) {
         this.setState({
-            report: props.report
+            report: props.report,
+            data: props.report && this.sheet2arr(props.report[2].Sheets[props.report[2].SheetNames[0]])
         })
     }
 
@@ -26,13 +29,46 @@ class PatientReportsView extends Component {
         return this.state.report && <Text>{this.state.report[0]}</Text>;
     }
 
+    sheet2arr(sheet){
+        var result = [];
+        var row;
+        var rowNum;
+        var colNum;
+        var range = XLSX.utils.decode_range(sheet['!ref']);
+        for(rowNum = range.s.r; rowNum <= range.e.r; rowNum++){
+           row = [];
+            for(colNum=range.s.c; colNum<=range.e.c; colNum++){
+               var nextCell = sheet[
+                  XLSX.utils.encode_cell({r: rowNum, c: colNum})
+               ];
+               if( typeof nextCell === 'undefined' ){
+                  row.push(void 0);
+               } else row.push(nextCell.w);
+            }
+            result.push(row);
+        }
+        return result;
+     };
+
     render() {
         var s = this.state;
         var p = s.patientData;
         return(
             <Card style={{height: '10000'}}>
                 {this.generatePatientReportsListView()}
-                {s.report && s.report[2].Sheets[s.report[2].SheetNames[0]].A1.v}
+                {s.report && 
+                    <LineChart width={800} height={800} data={this.state.data}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Line type="monotone" dataKey="0" stroke="#6644d8" />
+                        <Line type="monotone" dataKey="1" stroke="#6644e8" />
+                        <Line type="monotone" dataKey="2" stroke="#6644f8" />
+                        <Line type="monotone" dataKey="3" stroke="#4488d8" />
+                        <Line type="monotone" dataKey="4" stroke="#448888" />
+                        <Line type="monotone" dataKey="5" stroke="#448828" />
+                    </LineChart>
+                }
             </Card>
         );
     }
