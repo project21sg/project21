5019fa1ac6716a61e25c559dce6c1c54f92c71eb
Ze Xuan <syltaris@gmail.com>
2018-12-06 13:21:03 +0800

    implement simple patients view page

--- client/src/Views/Patients/view_patients_check.js
+++ client/src/Views/Patients/view_patients_check.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-
-
+import {
+    Row,
+    Col,
+    List,
+} from 'antd';
 import PatientsSummaryView from './view_patients_summary';
 
 var DUMMY_PATIENTS_DATA = [
@@ -79,38 +82,40 @@ class CheckPatientsView extends Component {
     constructor(props) {
         super();
         this.state = {
-            selectedTabId: "ng",
             patients: DUMMY_PATIENTS_DATA
         }
 
-        this.handleTabChange = this.handleTabChange.bind(this);
         this.generatePatientsListView = this.generatePatientsListView.bind(this);
     }
 
-    handleTabChange(e) {
-        this.setState((prevState) => {
-            return {
-                selectedTabId: e.selectedTabId
-            };
-        })
-    }
-
     generatePatientsListView() {
-        return this.state.patients && this.state.patients.map((p) => {}
-            // <Tab id={p.id} key ={p.name} title={p.name} panel={<PatientsSummaryView patientData={p}/>}/>
+        return (
+            <List
+            itemLayout="horizontal"
+            dataSource={this.state.patients}
+            renderItem={p => (
+                <List.Item>
+                    <List.Item.Meta
+                    title={p.name}
+                    />
+                </List.Item>
+            )}
+            />
         )
     }
 
     render() {
         var s = this.state;
         return(
-            <div>
-                12
-                {/* <Tabs vertical id="TabsExample" onChange={this.handleTabChange} selectedTabId={s.selectedTabId} style={{width: '10%'}}>
-                    {this.generatePatientsListView()}
-                    <Tabs.Expander />
-                    <input className="bp3-input" type="text" placeholder="Search..." />
-                </Tabs> */}
+            <div style={{margin: "5px 5px 5px 5px", padding: 20, height: "100vh", width:"100%", background: 'white'}}>
+                <Row>
+                    <Col span={3}>
+                        { this.generatePatientsListView() }
+                    </Col>
+                    <Col>
+                        Rest
+                    </Col>
+                </Row>
             </div>
         );
     }
