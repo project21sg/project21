4107debf6e88f2666a6416771b2b3f88100b9f1c
Ze Xuan <syltaris@gmail.com>
2019-01-18 17:03:24 +0800

    add inner labels to hori bar charts

--- client/src/Components/Charts/hori_labeled_bar.js
+++ client/src/Components/Charts/hori_labeled_bar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
     VictoryChart,
     VictoryBar,
+    VictoryLabel,
 } from 'victory';
 
 class HoriLabeledBar extends Component {
@@ -24,14 +25,17 @@ class HoriLabeledBar extends Component {
         var s = this.state;
         return(         
             <VictoryChart
-            domainPadding={{ y: 20 }}
+            domainPadding={{ y: 50 }}
             >
                 <VictoryBar horizontal
                     barWidth={30}
+                    data={ s.data.map((x) => {return {x: x.label, y: x.value}})  }
+                    labels={ (d) => d.y }
+                    labelComponent={<VictoryLabel dx={-40}/>}
                     style={{
                         data: { fill: "green" },
+                        labels: {fontSize: 20, fill: 'white'}
                     }}
-                    data={ s.data.map((x) => {return {x: x.label, y: x.value}})  }
                 />
             </VictoryChart>
         );
