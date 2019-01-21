61bac2ed38f67cb6d7a4530cc2acbd7b775608a4
Ze Xuan <syltaris@gmail.com>
2019-01-22 00:54:08 +0800

    slightly nicer line charts

--- client/src/Components/Charts/line_chart.js
+++ client/src/Components/Charts/line_chart.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {
-    VictoryChart,
-    VictoryLine,
-    VictoryLegend,
-} from 'victory';
+    LineChart,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Line,
+    Legend,
+    Tooltip
+} from 'recharts';
 
-class LineChart extends Component {
+class VerticalLineChart extends Component {
     constructor(props) {
         super();
         this.state = {
@@ -22,45 +26,34 @@ class LineChart extends Component {
     }
     //TODO: make this more modular, allow params for color, dynamic length inputs, 'selections', configs...
     _buildLines(data) {
-        var lineData = [];
-        for(var i= 0; i<6; i++) {
-            lineData.push(data.map((e) => {
-                return {
-                    x: e[6], y: e[i] 
-                };
-            }));
-        };
-        return lineData.map((y, i) =>
-            <VictoryLine
-            interpolation="natural"
-            data={y}
-            style={{
-                data: {
-                    stroke: i >= 3 ? 'lightblue' : 'pink'
-                }
-            }}
-            />
+        return data.map((y, i) =>
+            <Line type="monotone" dataKey="value" stroke="#8884d8" />
         );
     }
 
     render() {
         var s = this.state;
+        var data = [];
+        s.data.forEach((e) => data.push({name: e[6], ax: e[0], ay: e[1], az: e[2], gx: e[3], gy: e[4], gz: e[5]}))
         return(         
-            <VictoryChart
-            minDomain={{y: -5.0}}
-            maxDomain={{y: 5.0}}
-            >
-                { this._buildLines(s.data) }
-                <VictoryLegend 
-                title="Legend"
-                x={300} y={10}
-                data={[
-                    { name: "Gyroscope", symbol: { fill: "lightpink" } },
-                    { name: "Accelerometer", symbol: { fill: "lightblue" } },
-                  ]}
-                />
-            </VictoryChart>
+            <LineChart width={750} height={350} data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis/>
+                <Tooltip />
+                <Legend />
+                {/* {this._buildLines(data)} */}
+                <Line type="monotone" dataKey="ax" stroke="lightpink" />
+                <Line type="monotone" dataKey="ay" stroke="lightpink" />
+                <Line type="monotone" dataKey="az" stroke="lightpink" />
+                <Line type="monotone" dataKey="gx" stroke="lightblue" />
+                <Line type="monotone" dataKey="gy" stroke="lightblue" />
+                <Line type="monotone" dataKey="gz" stroke="lightblue" />
+
+
+            </LineChart>
         );
     }
 }
-export default LineChart;
+export default VerticalLineChart;
