74a87e37d343598250e7c396b806fd79e93b448b
Ze Xuan <syltaris@gmail.com>
2019-01-21 20:51:15 +0800

    allow timeout to reconnect to mongodb
    ecs doesn't have depends_on feature, need timeout in case db instance not created before server connections

--- server/db/mongodb.js
+++ server/db/mongodb.js
@@ -3,14 +3,27 @@ var mongooseOptions = { useMongoClient: true };
 
 require('../models/Patient');
 
-mongoose.connect('mongodb://mongodb:27017', mongooseOptions, function(err) {
+mongoose.connect('mongodb://localhost:27017', mongooseOptions, function(err) {
   if(err) {
-    console.log("Can't connect to mongodb:server; connecting to local mongo server...")
-    mongoose.connect('mongodb://localhost:27017', mongooseOptions, function(err) {
-      if(err) {
-        console.log("Can't connect to local mongodb:server; is the database active and online?")
-      }
-    }); //assuming it's local dev, BAD style 
+    console.log("Can't connect to mongodb:server @ localhost; retrying in 2 mins..")
+    console.log(err)
+
+    setTimeout(() => {
+      mongoose.connect('mongodb://localhost:27017', mongooseOptions, function(err) {
+        console.log('retried mongodb')
+        if(err) {
+          console.log("Can't connect to mongodb:server; connecting to docker named mongo server")
+          console.log(err)
+          mongoose.connect('mongodb://mongodb:27017', mongooseOptions, function(err) {
+            if(err) {
+              console.log("Can't connect to mongodb:server; is the database active and online?")
+            }
+          }); //assuming it's local dev, BAD style 
+        }
+      }); //assuming it's local dev, BAD style 
+    }, 72000)
+
+
   }
 });
 
