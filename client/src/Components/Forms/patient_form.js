79537e1d65a3e78fa3f60fea8d42585fa6c3d726
zexuan <syltaris@gmail.com>
2019-04-09 21:18:19 +0800

    fix client side api routes

--- client/src/Components/Forms/patient_form.js
+++ client/src/Components/Forms/patient_form.js
@@ -1,158 +1,155 @@
-import React, { Component } from 'react';
-import{
-    Row,
-    Col,
-    Button,
-    Form,
-    Input,
-    Radio,
-    Select,
-    DatePicker,
-    message
-} from 'antd';
+import React, { Component } from "react";
+import {
+  Row,
+  Col,
+  Button,
+  Form,
+  Input,
+  Radio,
+  Select,
+  DatePicker,
+  message
+} from "antd";
 
 class PatientForm extends Component {
-    constructor(props) {
-        super();
-        this.state = {
-            fields: props.fields,
-            waiting: false
-        };
-    }
+  constructor(props) {
+    super();
+    this.state = {
+      fields: props.fields,
+      waiting: false
+    };
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFieldsAndScroll((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-                this._uploadData(values);
-                this.props.history.push('/patients');
-            }
-        });
-    }
-    
-    //TODO: refactor into data logic container component (parent)
-    _uploadData(values) {
-        var data = {
-            patient: {
-                name: values.name,
-                nric: values.nric,
-                gender: values.gender, 
-                dateOfBirth: values['date of birth'],
-                contact: values.contact,
-                address: values.address, 
-                zipCode: values['zip code'],
-                occupation: values.occupation,
-                maritalStatus: values['marital status'],
-                knownHealthIssues: values['known health issues'],
-                nokName: values['next of kin name'],
-                nokRelation: values['relation to patient'],
-                nokAddress: values['next of kin address'],
-                nokContact: values['next of kin contact']
-            } 
-        };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log("Received values of form: ", values);
+        this._uploadData(values);
+        this.props.history.push("/patients");
+      }
+    });
+  };
 
-        this.setState({ waiting: true });
-        fetch(`http://${window.location.hostname}:9000/api/patient/`, {
-            method: 'post',
-            mode: 'cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }).then(function(json) {
-            console.log(json);
-            //this.setState({ waiting: false }); //need to set loading outside
-            message.success('Patient successfully added!');
-        }).catch((err) => { console.log(err);  if(err.status !== 200) message.error('Error occured while adding patient!'); }  )
-    }
+  //TODO: refactor into data logic container component (parent)
+  _uploadData(values) {
+    var data = {
+      name: values.name,
+      nric: values.nric,
+      gender: values.gender,
+      dateOfBirth: values["date of birth"],
+      contact: values.contact,
+      address: values.address,
+      zipCode: values["zip code"],
+      occupation: values.occupation,
+      maritalStatus: values["marital status"],
+      knownHealthIssues: values["known health issues"],
+      nokName: values["next of kin name"],
+      nokRelation: values["relation to patient"],
+      nokAddress: values["next of kin address"],
+      nokContact: values["next of kin contact"]
+    };
 
-    _buildFormItemView(field)  {
-        switch(field.inputType) {
-            case "radio":
-                return (
-                    <Radio.Group>
-                    {field.options.map((o) => 
-                        <Radio 
-                        key={o.toLowerCase()}
-                        value={o.toLowerCase()}>
-                            {o}
-                        </Radio>
-                    )}
-                    </Radio.Group>
-                )
-            case "datepicker":
-                return(
-                    <DatePicker 
-                    key={field.label}
-                    format="DD-MM-YYYY"/>
-                )
-            case "dropdown":
-                return(
-                    <Select
-                    key={field.label}>
-                    {field.options.map((o) => 
-                        <Select.Option 
-                        key={o.toLowerCase()}
-                        value={o.toLowerCase()}>
-                            {o}
-                        </Select.Option>
-                    )}
-                    </Select>
-                )
-            default:
-                return(
-                    <Input key={field.label} />
-                )
-        }
-    }
+    this.setState({ waiting: true });
+    fetch(`http://${window.location.hostname}:9000/api/v1/patients/`, {
+      method: "post",
+      mode: "cors",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    })
+      .then(function(json) {
+        console.log(json);
+        //this.setState({ waiting: false }); //need to set loading outside
+        message.success("Patient successfully added!");
+      })
+      .catch(err => {
+        console.log(err);
+        if (err.status !== 200)
+          message.error("Error occured while adding patient!");
+      });
+  }
 
-    _buildForm(fields, layout) {
-        return <Form onSubmit={this.handleSubmit}>
-        {fields.map((f) => 
-            <Form.Item
-            {...layout}
-            key={f.label}
-            label={f.label}>
-                {
-                    this.props.form.getFieldDecorator(f.label.toLowerCase(), f.validationConfig) ( this._buildFormItemView(f) )
-                }
-            </Form.Item>
-        )}
-        </Form>;
+  _buildFormItemView(field) {
+    switch (field.inputType) {
+      case "radio":
+        return (
+          <Radio.Group>
+            {field.options.map(o => (
+              <Radio key={o.toLowerCase()} value={o.toLowerCase()}>
+                {o}
+              </Radio>
+            ))}
+          </Radio.Group>
+        );
+      case "datepicker":
+        return <DatePicker key={field.label} format="DD-MM-YYYY" />;
+      case "dropdown":
+        return (
+          <Select key={field.label}>
+            {field.options.map(o => (
+              <Select.Option key={o.toLowerCase()} value={o.toLowerCase()}>
+                {o}
+              </Select.Option>
+            ))}
+          </Select>
+        );
+      default:
+        return <Input key={field.label} />;
     }
-    
-    render() {
-        var formItemLayout = {
-            labelCol: {
-                xs: { span: 24 },
-                sm: { span: 8 },
-            },
-            wrapperCol: {
-                xs: { span: 24 },
-                sm: { span: 16 },
-            }
-        }
+  }
 
-        return <div>
+  _buildForm(fields, layout) {
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        {fields.map(f => (
+          <Form.Item {...layout} key={f.label} label={f.label}>
+            {this.props.form.getFieldDecorator(
+              f.label.toLowerCase(),
+              f.validationConfig
+            )(this._buildFormItemView(f))}
+          </Form.Item>
+        ))}
+      </Form>
+    );
+  }
+
+  render() {
+    var formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
+      }
+    };
+
+    return (
+      <div>
         <Row>
-            {this.state.fields.map((f) => 
-                <Col span={10} style={{padding: "10px"}} key={f.header}>
-                    <span style={{fontWeight: 'bold', fontSize: 18}}>{f.header}</span>
-                    {this._buildForm(f.fields, formItemLayout)}
-                </Col>
-            )}
-        </Row>
-        <Row>
-            <Col 
-            span={20}
-            style={{textAlign: 'right'}}>
-                <Button type="primary" onClick={this.handleSubmit}>Submit</Button>
+          {this.state.fields.map(f => (
+            <Col span={10} style={{ padding: "10px" }} key={f.header}>
+              <span style={{ fontWeight: "bold", fontSize: 18 }}>
+                {f.header}
+              </span>
+              {this._buildForm(f.fields, formItemLayout)}
             </Col>
+          ))}
         </Row>
-        </div> 
-    }
+        <Row>
+          <Col span={20} style={{ textAlign: "right" }}>
+            <Button type="primary" onClick={this.handleSubmit}>
+              Submit
+            </Button>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 const BuiltPatientForm = Form.create()(PatientForm);
